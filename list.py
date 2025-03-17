9dd219b01812227d457efbfb990b95e8f2c0b5f4
Lenalepisho <lenajonathan02@gmail.com>
2025-03-18 00:27:34 +0300

    new changes

---
+++ list.py
@@ -0,0 +1,5 @@
+mbogi = ["bright", "Saroi", "Jimy", "Margaret", 5, True]
+print(mbogi)
+print(mbogi[2])
+
+telegram 
--- python.py
+++ python.py
@@ -1,13 +1,25 @@
 print("hello world")
 
-# boolean
+# Data Variables-Container(storage area)  to hold a data 
+# number=10 @ number is the variable storing value 10
+# download cursor AI and sign in
+# Datatype =specify type of data that can be stored indside 
+ # boolean
 # String
 # float
 # integer
 
+name= "Makandura"
+dev= True
+print(name)
+
+
+
+
+
 # print(type(name))
 
-# Dynamic Typing: Variables changing type
-year=2025
-year="twenty twenty five"
-print(type(year), year)
+# # Dynamic Typing: Variables changing type
+# year=2025
+# year="twenty twenty five"
+# print(type(year), year)
